e59be63d721372bd1d3afba059f096b9066b2ad6
marcho001 <g40419@gmail.com>
2023-05-07 15:31:36 +0800

    jest 設定

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 export default {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   // testRegex: '(.*|(\\.|/)(test|spec))\\.tsx?$',
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
--- src/pages/Home/test/Index.spec.tsx
+++ src/pages/Home/test/Index.spec.tsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
 
 // To Test
 import Home from '../Index'
